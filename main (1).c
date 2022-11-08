4382605038609ec016e8225581f03d259b57f1d0
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2022-11-07 21:01:39 -0300

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,15 @@
+// Implemente uma função, sem retorno, que recebe um valor inteiro por parâmetro e
+//informa se é, ou não, igual a zero.
+#include <stdio.h>
+ int main(){
+    int p,v;
+    printf("informe um numero:");
+    scanf("%d", &p);
+    if (p<0){
+        printf("não é igual a zero:");
+    }
+     else{
+         printf("é igual a zero");
+    } 
+     
+ }
